b1425dbf97103cf96de119595e1cca854f8bfde6
Iliyan Antov <i.antov2@gmail.com>
2020-01-26 02:22:30 +0200

    Using TinyGSM library to operate GPRS module

--- TrafficSignCode/MicrocontrollerCode/MicrocontrollerCode.ino
+++ TrafficSignCode/MicrocontrollerCode/MicrocontrollerCode.ino
@@ -1,48 +1,155 @@
+#define TINY_GSM_MODEM_SIM800
+
+#define TINY_GSM_DEBUG SerialMon
+
+// Range to attempt to autobaud
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 115200
+
+// Uncomment this if you want to use SSL
+//#define USE_SSL
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// Set serial for AT commands (to the module)
+// Use Hardware Serial on Mega, Leonardo, Micro
+#define SerialAT SoftwareSerial
+
 #include <SoftwareSerial.h>
+SoftwareSerial SerialAT(3, 2); // RX, TX
 
-//Create software serial object to communicate with SIM800L
-SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
 
-void setup()
-{
-  //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
-  Serial.begin(9600);
-  
-  //Begin serial communication with Arduino and SIM800L
-  mySerial.begin(9600);
+// set GSM PIN, if any
+#define GSM_PIN ""
 
-  Serial.println("Initializing...");
-  delay(1000);
+// Your GPRS credentials
+// Leave empty, if missing user or pass
+const char apn[]  = "internet.vivacom.com";
+const char gprsUser[] = "VIVACOM";
+const char gprsPass[] = "VIVACOM";
 
-  mySerial.println("AT");
-  updateSerial();
-  mySerial.println("AT+CFUN=1");
-  updateSerial();
-  mySerial.println("AT+CSTT=\"internet.vivacom.com\",\"VIVACOM\",\"VIVACOM\"");
-  updateSerial();
-  mySerial.println("AT+CIICR");
-  updateSerial();
-  mySerial.println("AT+CIPSTART=\"TCP\",\"37.157.168.186\",65432");
-  updateSerial();
- 
+// Server details
+const char server[] = "vsh.pp.ua";
+const char resource[] = "/TinyGSM/logo.txt";
+
+#include <TinyGsmClient.h>
+
+TinyGsm modem(SerialAT);
+
+#ifdef USE_SSL
+  TinyGsmClientSecure client(modem);
+  const int  port = 443;
+#else
+  TinyGsmClient client(modem);
+  const int  port = 80;
+#endif
+
+void setup() {
+  // Set console baud rate
+  SerialMon.begin(115200);
+  delay(10);
+
+  // Set your reset, enable, power pins here
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+
+  SerialMon.println("Wait...");
+
+  // Set GSM module baud rate
+  // TinyGsmAutoBaud(SerialAT,GSM_AUTOBAUD_MIN,GSM_AUTOBAUD_MAX);
+  SerialAT.begin(9600);
+  delay(3000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // modem.init();
+
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print("Modem: ");
+  SerialMon.println(modemInfo);
+
+#if TINY_GSM_USE_GPRS
+  // Unlock your SIM card with a PIN if needed
+  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(GSM_PIN);
+}
+#endif
 }
 
-void loop()
-{
-  updateSerial();
-}
+void loop() {
 
-void updateSerial()
-{
-  delay(500);
-  while (Serial.available()) 
-  {
-    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+#if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_XBEE
+  // The XBee must run the gprsConnect function BEFORE waiting for network!
+  modem.gprsConnect(apn, gprsUser, gprsPass);
+#endif
+
+  SerialMon.print("Waiting for network...");
+  if (!modem.waitForNetwork(240000L)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
   }
-  while(mySerial.available()) 
-  {
-    int data = 0;
-    data = mySerial.read();
-    Serial.write(data);//Forward what Software Serial received to Serial Port
+  SerialMon.println(" OK");
+
+  if (modem.isNetworkConnected()) {
+    SerialMon.println("Network connected");
+  }
+
+#if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_HAS_GPRS
+    SerialMon.print(F("Connecting to "));
+    SerialMon.print(apn);
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+      SerialMon.println(" fail");
+      delay(10000);
+      return;
+    }
+    SerialMon.println(" OK");
+#endif
+
+  SerialMon.print("Connecting to ");
+  SerialMon.println(server);
+  if (!client.connect(server, port)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" OK");
+
+  // Make a HTTP GET request:
+  SerialMon.println("Performing HTTP GET request...");
+  client.print(String("GET ") + resource + " HTTP/1.1\r\n");
+  client.print(String("Host: ") + server + "\r\n");
+  client.print("Connection: close\r\n\r\n");
+  client.println();
+
+  unsigned long timeout = millis();
+  while (client.connected() && millis() - timeout < 10000L) {
+    // Print available data
+    while (client.available()) {
+      char c = client.read();
+      SerialMon.print(c);
+      timeout = millis();
+    }
+  }
+  SerialMon.println();
+
+  // Shutdown
+
+  client.stop();
+  SerialMon.println(F("Server disconnected"));
+
+#if TINY_GSM_USE_GPRS
+    modem.gprsDisconnect();
+    SerialMon.println(F("GPRS disconnected"));
+#endif
+
+  // Do nothing forevermore
+  while (true) {
+    delay(1000);
   }
 }
